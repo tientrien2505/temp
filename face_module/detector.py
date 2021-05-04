426cbd0f46fc03bd7f82d102469caf6cd9ac6189
truongdtnms <truongdt@nms.com.vn>
2021-05-04 14:41:11 +0700

    mtcnn detector

--- face_module/detector.py
+++ face_module/detector.py
@@ -1,6 +1,4 @@
 from abc import ABC, abstractmethod
-from face_recognition import face_locations
-from insightface.model_zoo import get_model
 
 class Detector(ABC):
     def __init__(self):
@@ -15,6 +13,7 @@ class HogDetector(Detector):
         super(HogDetector, self).__init__()
 
     def detect(self, img):
+        from face_recognition import face_locations
         _face_locations = face_locations(img, model='hog')
         return _face_locations
 
@@ -23,12 +22,14 @@ class CnnDetector(Detector):
         super(CnnDetector, self).__init__()
 
     def detect(self, img):
+        from face_recognition import face_locations
         _face_locations = face_locations(img, model='cnn')
         return _face_locations
 
 
 class RetinaFaceDetector(Detector):
     def __init__(self, model_name='retinaface_r50_v1', gpu=False):
+        from insightface.model_zoo import get_model
         self._model = get_model(model_name)
         _ctx_id = 1 if gpu else -1
         self._model.prepare(ctx_id=_ctx_id, nms=0.4)
@@ -39,11 +40,27 @@ class RetinaFaceDetector(Detector):
         _face_locations = _face_locations[:, [1, 2, 3, 0]]
         return _face_locations
 
+class MtcnnDetector(Detector):
+    def __init__(self):
+        super(MtcnnDetector, self).__init__()
+        from mtcnn import MTCNN
+        self._model = MTCNN()
+
+    def detect(self, img):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        import time
+        start = time.time()
+        faces = self._model.detect_faces(img)
+        print(time.time()-start)
+        rs = [[face['box'][1], face['box'][0] + face['box'][2], face['box'][1] + face['box'][3], face['box'][0]] for face in faces]
+        return rs
+
 class DetectorBuilder():
     detectors = {
         'cnn': CnnDetector,
         'hog': HogDetector,
-        'retina': RetinaFaceDetector
+        'retina': RetinaFaceDetector,
+        'mtcnn': MtcnnDetector
     }
     def __init__(self):
         pass
@@ -55,8 +72,8 @@ class DetectorBuilder():
 
 if __name__ == '__main__':
     import cv2
-    dt = DetectorBuilder.build('retina')
-    img = cv2.imread('/home/truongdt/projects/faceid/son_face_core/test.jpg')
+    dt = DetectorBuilder.build('mtcnn')
+    img = cv2.imread('./test.jpg')
     det = dt.detect(img)
     for d in det:
         top, right, bottom, left = d
